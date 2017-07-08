f48c96a02b1df33e305245c094db51327f09ffef
Zaku <tomle.zaku@gmail.com>
2017-07-08 14:47:03 +0700

    Change location in Gradient Background

--- example/shimmer.js
+++ example/shimmer.js
@@ -1,256 +1,116 @@
-/* @flow */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, { Component } from 'react';
 
-import React, { PureComponent, Component } from 'react';
-import { View, Text, StyleSheet, Animated, Image, ScrollView } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  LayoutAnimation,
+  Platform,
+  UIManager,
+} from 'react-native';
 
-import ShimmerPlaceHolder from './ShimmerPlaceholder.js';
+  var CustomLayoutAnimation = {
+    duration: 200,
+    create: {
+      type: LayoutAnimation.Types.linear,
+      property: LayoutAnimation.Properties.opacity,
+    },
+    update: {
+      type: LayoutAnimation.Types.curveEaseInEaseOut,
+    },
+  };
 
-class Line extends PureComponent {
-  render() {
+export default class App extends Component {
+
+  constructor() {
+    super();
+    if (Platform.OS === 'android') {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+    this.state = {
+      index: 0,
+    }
+  }
+
+  onPress(index) {
+
+    // Uncomment to animate the next state change.
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+
+    // Or use a Custom Layout Animation
+    // LayoutAnimation.configureNext(CustomLayoutAnimation);
+
+    this.setState({index: index});
+  }
+
+  renderButton(index) {
     return (
-      <View style={[{
-        height: 3,
-        backgroundColor: 'rgba(154, 154, 154, 0.29)',
-        overflow: 'hidden',
-      }, this.props.style]}
-      >
-        <View style={[this.props.style]} />
-      </View>
+      <TouchableOpacity key={'button' + index} style={styles.button} onPress={() => this.onPress(index)}>
+        <Text>{index}</Text>
+      </TouchableOpacity>
     );
   }
-}
 
-export default class Shimmer extends Component {
-  constructor(props) {
-    super(props);
-    this.loadingAnimated = [];
-    this.avatarLoadingAnimated = [];
-    this.animatedAvatarReverseLoading = [];
-    this.bigImageAndSomeRowsAnimated = [];
-    this.state = {
-      isfetched: false,
-      imageIsFetched: false,
-    };
-  }
-  componentDidMount() {
-    this.runAnimated();
-    this.runAvatarAnimated();
-    this.runAvatarReverseAnimated();
-    this.runBigAvatarAndSomeRowsAnimated();
-    setTimeout(() => this.setState({ isfetched: true }), 2000);
-  }
-
-  runAnimated() {
-    if (Array.isArray(this.loadingAnimated) && this.loadingAnimated.length > 0) {
-      const threeRowAnimated = Animated.parallel(
-        this.loadingAnimated.map(animate => {
-          if (animate && animate.runAnimated) {
-            return animate.runAnimated();
-          }
-          return null;
-        }),
-        {
-          stopTogether: false,
-        },
-      );
-      Animated.loop(threeRowAnimated).start();
-    }
-  }
-  runAvatarReverseAnimated() {
-      console.log('====================================');
-      console.log('....', this.animatedAvatarReverseLoading);
-      console.log('====================================');
-    if (Array.isArray(this.animatedAvatarReverseLoading) && this.animatedAvatarReverseLoading.length > 0) {
-      const sequenceReverseAnimated = Animated.sequence([
-        this.animatedAvatarReverseLoading[0].runAnimated(), // image left
-        Animated.parallel( //4 row middle
-          this.animatedAvatarReverseLoading.slice(1, this.animatedAvatarReverseLoading.length-1).map(animate => {
-            if (animate && animate.runAnimated) {
-              return animate.runAnimated();
-            }
-            return null;
-          }), 
-          {
-            stopTogether: false,
-          },
-        ),
-        this.animatedAvatarReverseLoading[this.animatedAvatarReverseLoading.length - 1].runAnimated(), // right image
-      ]
-    )
-    .start(()=>this.runAvatarReverseAnimated())
-    // Animated.loop(sequenceReverseAnimated).start()
-    }
-  }
-  runBigAvatarAndSomeRowsAnimated() {
-    if (Array.isArray(this.bigImageAndSomeRowsAnimated) && this.bigImageAndSomeRowsAnimated.length > 0) {
-      Animated.parallel(
-        [
-          this.bigImageAndSomeRowsAnimated[0].runAnimated(),
-          ...this.bigImageAndSomeRowsAnimated.slice(1).map(animate => {
-          if (animate && animate.runAnimated) {
-            return animate.runAnimated();
-          }
-            return null;
-          }),
-        ],
-        {
-          stopTogether: false,
-        }).start(() => {
-          this.runBigAvatarAndSomeRowsAnimated();
-    })
-    }
-  }
-  runAvatarAnimated() {
-    if (Array.isArray(this.avatarLoadingAnimated) && this.avatarLoadingAnimated.length > 0) {
-      const avatarandrowsAnimated = Animated.sequence([
-        this.avatarLoadingAnimated[0].runAnimated(),
-        Animated.parallel(
-          this.avatarLoadingAnimated.slice(1).map(animate => {
-            if (animate&&animate.runAnimated) {
-              return animate.runAnimated();
-            }
-            return null
-          }),
-          {
-            stopTogether: false
-          }
-        )]
-      );
-      Animated.loop(avatarandrowsAnimated).start();
-    }
-  }
-  _renderRows(loadingAnimated,number,uniqueKey){
-    shimmerRows=[]
-    for(let index=0;index<number;index++ ){
-      shimmerRows.push(
-        <ShimmerPlaceHolder
-          key={`loading-${index}-${uniqueKey}`}
-          ref = {(ref) => loadingAnimated.push(ref)}
-          style={{ marginBottom: 7 }}
-        />
-      )
-    }
-    return(
-      <View>
-        {shimmerRows}
-      </View>
-    )
-  }
-  _renderRowsReverse(loadingAnimated,number,uniqueKey){
-    shimmerRows=[]
-    for(let index=0;index<number;index++ ){
-      shimmerRows.push(
-        <ShimmerPlaceHolder
-          key={`loading-${index}-${uniqueKey}`}
-          ref={(ref) => loadingAnimated.push(ref)}
-          width={150}
-          style={{ marginBottom: 7}}
-          reverse
-
-        />
-      )
-    }
-    return(
-      <View>
-        {shimmerRows}
-      </View>
-    )
-  }
-  _renderImageAndRows = () => {
-    this.avatarLoadingAnimated=[]
+  renderCircle(key) {
+    var size = 50;
     return (
-      <View>
-        <View style={{ flexDirection: 'row' }}>
-          <ShimmerPlaceHolder
-            ref={(ref) => this.avatarLoadingAnimated.push(ref)}
-            width={60}
-            height={60}
-            style={{ marginRight: 16 }}
-          />
-          {this._renderRows(this.avatarLoadingAnimated, 3, 'image-row')}
-        </View>
-      </View>
-    )
+      <View key={key} style={{width: size, height: size, borderRadius: size / 2.0, backgroundColor: 'sandybrown', margin: 20}}/>
+    );
   }
-  _renderReverseAnimated= () => {
-    // this.animatedAvatarReverseLoading= []
-    return(
-      <View style={{ flexDirection: 'row-reverse' }}>
-        <ShimmerPlaceHolder
-          ref={(ref) => this.animatedAvatarReverseLoading.push(ref)}
-          width={60}
-          height={60}
-          style={{ marginLeft: 16, }}
-          reverse
-        />
-        <View>
-          {this._renderRowsReverse(this.animatedAvatarReverseLoading, 3, 'reverse')}
-        </View>
-        <ShimmerPlaceHolder
-          ref={(ref) => this.animatedAvatarReverseLoading.push(ref)}
-          width={60}
-          height={60}
-          style={{ marginLeft: 16, marginRight: 16 }}
-          reverse
-        />
-      </View>
-    )
-  }
+
   render() {
-    const { isfetched, imageIsFetched } = this.state;
+
+    var leftStyle = this.state.index === 0 ? {flex: 1} : {width: 20};
+    var middleStyle = this.state.index === 2 ? {width: 20} : {flex: 1};
+    var rightStyle = {flex: 1};
+
+    var whiteHeight = this.state.index * 80;
+
+    var circles = [];
+    for (var i = 0; i < (5 + this.state.index); i++) {
+      circles.push(this.renderCircle(i));
+    }
+
     return (
-      <ScrollView style={styles.container}>
-        <Text style={{ marginBottom: 7 }}> Simple </Text>
-        <ShimmerPlaceHolder autoRun={true} />
-        <Line style={{ marginTop: 7, marginBottom: 7, paddingLeft: 16 }} />
-        <Text style={{ marginBottom: 7 }}> 3 Rows </Text>
-        {this._renderRows(this.loadingAnimated, 3, 'three-rows')}
-        <Line style={{ marginTop: 7, marginBottom: 7, paddingLeft: 16 }} />
-        <Text style={{ marginBottom: 7 }}> Image and Rows </Text>
-        {this._renderImageAndRows()}
-        <Line style={{ marginTop: 7, marginBottom: 7, paddingLeft: 16 }} />
-        <Text style={{ marginBottom: 7 }}>Reverse Animated</Text>
-        {this._renderReverseAnimated()}
-
-        <Line style={{ marginTop: 7, marginBottom: 7, paddingLeft: 16 }} />
-        <Text style={{ marginBottom: 7 }}>Fetching image and text</Text>
-
-        <View style={{ alignItems: 'center' }} >
-          <ShimmerPlaceHolder
-            ref={(ref) => this.bigImageAndSomeRowsAnimated.push(ref)}
-            width={175}
-            height={175}
-            isDisplayChildComponent={imageIsFetched}
-          >
-            <Image
-              style={{ width: 175, height: 175 }}
-              source={{ uri: 'https://unsplash.it/300/300' }}
-              onLoad={() => { this.setState({ imageIsFetched: true }); }}
-
-            />
-          </ShimmerPlaceHolder>
-          <View>
-            <ShimmerPlaceHolder
-              ref={(ref) => this.bigImageAndSomeRowsAnimated.push(ref)}
-              style={{ marginTop: 7 }}
-              width={350}
-              height={9}
-              isDisplayChildComponent={isfetched}
-            >
-              <Text style={{ marginTop: 3 }}>Lorem Ipsum is simply dummy text of the printing</Text>
-            </ShimmerPlaceHolder>
-            <ShimmerPlaceHolder
-              ref={(ref) => this.bigImageAndSomeRowsAnimated.push(ref)}
-              style={{ marginTop: 7 }}
-              width={350}
-              height={9}
-              isDisplayChildComponent={isfetched}
-            >
-              <Text style={{ marginTop: 3 }}>Lorem Ipsum is simply dummy text of the printing </Text>
-            </ShimmerPlaceHolder>
+      <View style={styles.container}>
+        <View style={styles.topButtons}>
+          {this.renderButton(0)}
+          {this.renderButton(1)}
+          {this.renderButton(2)}
+        </View>
+        <View style={styles.content}>
+          <View style={{flexDirection: 'row', height: 100}}>
+            <View style={[leftStyle, {backgroundColor: 'firebrick'}]}/>
+            <View style={[middleStyle, {backgroundColor: 'seagreen'}]}/>
+            <View style={[rightStyle, {backgroundColor: 'steelblue'}]}/>
+          </View>
+          <View style={{flexDirection: 'row', height: 100}}>
+            <View style={[leftStyle, {backgroundColor: 'firebrick'}]}/>
+            <View style={[middleStyle, {backgroundColor: 'seagreen'}]}/>
+            <View style={[rightStyle, {backgroundColor: 'steelblue'}]}/>
+          </View>
+          <View style={{flexDirection: 'row', height: 100}}>
+            <View style={[leftStyle, {backgroundColor: 'firebrick'}]}/>
+            <View style={[middleStyle, {backgroundColor: 'seagreen'}]}/>
+            <View style={[rightStyle, {backgroundColor: 'steelblue'}]}/>
+          </View>
+          <View style={{height: whiteHeight, justifyContent: 'center', alignItems: 'center', overflow: 'hidden'}} removeClippedSubviews={true}>
+            <View>
+              <Text>Stuff Goes Here</Text>
+            </View>
+          </View>
+          <View style={styles.circleContainer}>
+            {circles}
           </View>
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
@@ -258,7 +118,38 @@ export default class Shimmer extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 16,
-    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  topButtons: {
+    marginTop: 22,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: 'lightblue',
+  },
+  button: {
+    flex: 1,
+    height: 60,
+    alignSelf: 'stretch',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 8,
+  },
+  content: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  circleContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    flexWrap: 'wrap',
+    padding: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
+
