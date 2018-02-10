b48d059a7ea876297834320c013ceacb0ac339fe
Zaku <tomle.zaku@gmail.com>
2018-02-10 19:52:12 +0700

    Update RM + delete expo

--- src/index.js
+++
@@ -1,7 +0,0 @@
-
-import ShimmerPlaceHolder from './ShimmerPlaceholder'
-
-import ShimmerPlaceholderExpo from './ShimmerPlaceholder.expo'
-
-export default ShimmerPlaceHolder;
-export const ShimmerPlaceholderExpo;
