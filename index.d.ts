d8a138300619ca7e43d167dce11fe8026fa7f90a
Tri Le <tri.le@saigontechnology.com>
2019-04-07 09:04:04 +0700

    Update typescript

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,21 @@
-declare module 'react-native-shimmer-placeholder';
+declare module 'react-native-shimmer-placeholder' {
+    import React from 'react';
+    type Props = {
+        width?: number | string,
+        height?: number | string,
+        widthShimmer?: number,
+        duration?: number,
+        delay?: number,
+        colorShimmer?: string[],
+        reverse?: boolean,
+        autoRun?: boolean,
+        visible?: boolean,
+        children?: any,
+        style?: any,
+        backgroundColorBehindBorder?: string,
+        hasBorder?: boolean,
+        isInteraction?: boolean,
+    }
+    declare const MyComponent: React.SFC<Props>
+    export default MyComponent
+}
