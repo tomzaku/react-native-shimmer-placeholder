c807ecbe10fd0fce7c8e1ccfbb9d0bec57e168e9
Miron Pawlik <miron.pawlik@swmansion.com>
2019-05-09 10:26:21 +0200

    Add getAnimated to TS types (and use interface)

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,23 @@
 declare module 'react-native-shimmer-placeholder' {
     import React from 'react';
-    type Props = {
-        width?: number | string,
-        height?: number | string,
-        widthShimmer?: number,
-        duration?: number,
-        delay?: number,
-        colorShimmer?: string[],
-        reverse?: boolean,
-        autoRun?: boolean,
-        visible?: boolean,
-        children?: any,
-        style?: any,
-        backgroundColorBehindBorder?: string,
-        hasBorder?: boolean,
-        isInteraction?: boolean,
+    import { Animated } from 'react-native';
+    interface Props {
+        width?: number | string;
+        height?: number | string;
+        widthShimmer?: number;
+        duration?: number;
+        delay?: number;
+        colorShimmer?: string[];
+        reverse?: boolean;
+        autoRun?: boolean;
+        visible?: boolean;
+        children?: any;
+        style?: any;
+        backgroundColorBehindBorder?: string;
+        hasBorder?: boolean;
+        isInteraction?: boolean;
     }
-    declare class MyComponent extends React.Component<Props, any> {}
-    export default MyComponent
+    export default class ShimmerPlaceHolder extends React.Component<Props, any> {
+        getAnimated(): Animated.CompositeAnimation;
+    }
 }
